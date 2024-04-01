0d730fe5fca5d08cbdc612e15680d7b3489377b0
unknown <sebaadamoszek@vp.pl>
2024-04-01 15:35:05 +0200

    poprawa homepage

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/Projekt-Kapusta-Frontend">
+        <BrowserRouter basename="/Kapusta">
           <App />
         </BrowserRouter>
       </PersistGate>
