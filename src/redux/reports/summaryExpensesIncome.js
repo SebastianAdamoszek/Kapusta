2864421f499649f707425c4a81db5aebf50f2e6e
SebastianAdamoszek <sebaadamoszek@vp.pl>
2024-04-09 21:18:24 +0200

    sprawdzanie baseURL

--- src/redux/reports/summaryExpensesIncome.js
+++ src/redux/reports/summaryExpensesIncome.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 // axios.defaults.baseURL = 'https://kapusta-a0a137454a45.herokuapp.com/';
 // axios.defaults.baseURL = 'http://localhost:4000';
-axios.defaults.baseURL = 'https://kapusta-backend.netlify.app';
 
 export const downloadSummaryExpenses = async () => {
   try {
